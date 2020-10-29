6c22fd3e645292139d737e42b0a8e0a92bb53336
Aiden Vigue <9821808+acvigue@users.noreply.github.com>
2020-10-29 19:03:02 -0400

    Setup PKCE

--- src/main.js
+++ src/main.js
@@ -4,11 +4,32 @@ import router from './router'
 import {Capacitor, Plugins} from "@capacitor/core"
 import './quasar'
 import LogRocket from "logrocket"
+const axios = require("axios").default;
+const qs = require('querystring')
 LogRocket.init("tefu1d/scrobble");
 
 const {Storage, Browser} = Plugins;
 const NativeApp = Plugins.App;
 Vue.config.productionTip = false
+
+async function generateCodeChallenge(codeVerifier) {
+  var digest = await crypto.subtle.digest("SHA-256",
+      new TextEncoder().encode(codeVerifier));
+
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+      .replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+}
+
+function generateRandomString(length) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
 
 new Vue({
   router,
@@ -21,26 +42,61 @@ new Vue({
       } else {
         redirect_uri = "http://" + window.location.hostname + ":8080/spcallback"
       }
+      const codeVerifier = generateRandomString(64);
+      const codeChallenge = generateCodeChallenge(codeVerifier);
       const scopes = "user-read-private user-read-email user-read-playback-state user-read-currently-playing";
       const url = 'https://accounts.spotify.com/authorize' +
-      '?response_type=code' +
+      '?response_type=code&code_challenge_method=S256' +
+      '&code_challenge=' + encodeURIComponent(codeChallenge);
       '&client_id=' + "30ff94fb26df46dbab49759ddd32cc1c" +
       (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
       '&redirect_uri=' + encodeURIComponent(redirect_uri);
       Browser.open({url: url});
-      Browser.addListener('browserPageLoaded', (data) => {
-        console.log('Data - browserPageLoaded: '+JSON.stringify(data));
-        LogRocket.captureMessage('Data - browserPageLoaded: '+JSON.stringify(data));
+      NativeApp.addListener("appUrlOpen", (data) => {
+        LogRocket.captureMessage('Data - appUrlOpen: '+JSON.stringify(data));
+        const url = data.url.replace("scrobble://", "");
+        if(url.indexOf("spcallback") != -1) {
+          const spAuthCode = url.split("?code=")[1];
+          this.spotify.grantCode = spAuthCode;
+          Browser.close();
+          this.$q.loading.show();
+
+          //Authorize with Spotify!
+          const requestBody = {
+            client_id: "30ff94fb26df46dbab49759ddd32cc1c",
+            grant_type: "authorization_code",
+            code: spAuthCode,
+            redirect_uri: redirect_uri,
+            code_verifier: codeVerifier
+          }
+          
+          const config = {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          }
+          
+          axios.post("https://accounts.spotify.com/api/token", qs.stringify(requestBody), config)
+          .then((result) => {
+            // Do somthing
+            LogRocket.captureMessage('Data - spresult: '+JSON.stringify(result));
+            this.$q.loading.hide();
+          })
+          .catch((err) => {
+            // Do somthing
+            LogRocket.captureMessage('Data - sperror: '+JSON.stringify(err));
+            this.$q.loading.hide();
+          })
+        }
       });
     }
   },
   data: function() {
     return {
-      authed: false
+      authed: false,
+      spotify: {
+        grantCode: ""
+      }
     }
   }
 }).$mount('#app')
-
-NativeApp.addListener("appUrlOpen", (data) => {
-  LogRocket.captureMessage('Data - appUrlOpen: '+JSON.stringify(data));
-});
--- src/quasar.js
+++ src/quasar.js
@@ -3,10 +3,11 @@ import Vue from 'vue'
 import './styles/quasar.sass'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
-import { Quasar } from 'quasar'
+import { Quasar , Loading } from 'quasar'
 
 Vue.use(Quasar, {
   config: {},
   plugins: {
+    Loading
   }
  })
