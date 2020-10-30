41b7f3ed72201467d172eec5db862b62329ca3f3
Aiden Vigue <acvigue@me.com>
2020-10-30 15:52:14 -0400

    10/30/20

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ import LogRocket from "logrocket"
 const axios = require("axios").default;
 var CryptoJS = require("crypto-js");
 const qs = require('querystring')
-LogRocket.init("tefu1d/scrobble");
+//LogRocket.init("tefu1d/scrobble");
 
-const {Storage, Browser} = Plugins;
+const {Storage, Browser, Toast} = Plugins;
 const NativeApp = Plugins.App;
 Vue.config.productionTip = false
 
 function generateCodeChallenge(code_verifier) {
-  return code_challenge = base64URL(CryptoJS.SHA256(code_verifier))
+  return base64URL(CryptoJS.SHA256(code_verifier))
 }
 function base64URL(string) {
   return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
@@ -43,6 +43,8 @@ new Vue({
         redirect_uri = "http://" + window.location.hostname + ":8080/spcallback"
       }
       const codeVerifier = generateRandomString(74);
+      window.localStorage.setItem("rduri", redirect_uri);
+      window.localStorage.setItem("verifier", codeVerifier);
       const codeChallenge = generateCodeChallenge(codeVerifier);
       const scopes = "user-read-private user-read-email user-read-playback-state user-read-currently-playing";
       const url = 'https://accounts.spotify.com/authorize' +
@@ -51,52 +53,78 @@ new Vue({
       '&client_id=' + "30ff94fb26df46dbab49759ddd32cc1c" +
       (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
       '&redirect_uri=' + encodeURIComponent(redirect_uri);
-      Browser.open({url: url});
-      NativeApp.addListener("appUrlOpen", (data) => {
-        LogRocket.captureMessage('Data - appUrlOpen: '+JSON.stringify(data));
-        const url = data.url.replace("scrobble://", "");
-        if(url.indexOf("spcallback") != -1) {
-          const spAuthCode = url.split("?code=")[1];
-          this.spotify.grantCode = spAuthCode;
-          Browser.close();
-          this.$q.loading.show();
-
-          //Authorize with Spotify!
-          const requestBody = {
-            client_id: "30ff94fb26df46dbab49759ddd32cc1c",
-            grant_type: "authorization_code",
-            code: spAuthCode,
-            redirect_uri: redirect_uri,
-            code_verifier: codeVerifier
+      if(Capacitor.isNative) {
+        Browser.open({url: url});
+        NativeApp.addListener("appUrlOpen", (data) => {
+          //LogRocket.captureMessage('Data - appUrlOpen: '+JSON.stringify(data));
+          const url = data.url.replace("scrobble://", "");
+          if(url.indexOf("spcallback") != -1) {
+            const spAuthCode = url.split("?code=")[1].split("#/")[0];
+            this.spotify.grantCode = spAuthCode;
+            Browser.close();
+            this.getSpotifyTokens(codeVerifier, spAuthCode, redirect_uri);
           }
-          
-          const config = {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
-          }
-          
-          axios.post("https://accounts.spotify.com/api/token", qs.stringify(requestBody), config)
-          .then((result) => {
-            // Do somthing
-            LogRocket.captureMessage('Data - spresult: '+JSON.stringify(result));
-            this.$q.loading.hide();
-          })
-          .catch((err) => {
-            // Do somthing
-            LogRocket.captureMessage('Data - sperror: '+JSON.stringify(err));
-            this.$q.loading.hide();
-          })
+        });
+      } else {
+        window.location.href = url;
+      }
+    },
+    getSpotifyTokens: function(verifier, code, redirect_uri) {
+      //Authorize with Spotify!
+      this.$q.loading.show();
+      const requestBody = {
+        client_id: "30ff94fb26df46dbab49759ddd32cc1c",
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: redirect_uri,
+        code_verifier: verifier
+      }
+      
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
-      });
+      }
+      
+      axios.post("https://accounts.spotify.com/api/token", qs.stringify(requestBody), config)
+      .then(async (result) => {
+        console.log(result);
+        Toast.show({
+          text: "Connected to Spotify!"
+        })
+        this.spotify.accessToken = result.data.access_token;
+        this.spotify.refreshToken = result.data.refresh_token;
+        await Storage.set({key: "spotify_auth", value: JSON.stringify(this.spotify)});
+        this.authed = true;
+        this.authProvider = "spotify";
+        this.$q.loading.hide();
+      })
+      .catch((err) => {
+        console.error(err);
+        Toast.show({
+          text: "Couldn't authorize with Spotify."
+        })
+        this.$q.loading.hide();
+      })
     }
   },
   data: function() {
     return {
       authed: false,
+      authProvider: "",
       spotify: {
-        grantCode: ""
+        grantCode: "",
+        accessToken: "",
+        refreshToken: ""
       }
+    }
+  },
+  mounted: async function() {
+    if(window.location.pathname.indexOf("spcallback") != -1 && Capacitor.isNative == false) {
+      const codeVerifier = window.localStorage.getItem("verifier");
+      const redirect_uri = window.localStorage.getItem("rduri");
+      const authCode = window.location.href.split("code=")[1].split("#/")[0];
+      this.getSpotifyTokens(codeVerifier, authCode, redirect_uri);
     }
   }
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,11 @@
       <q-btn size='lg' style="background-color: #1DB954; color: white;" @click="$root.openSpotifyAuth">
         <img src="Spotify_Icon_RGB_White.png" height=35px; style="margin-right: 10px;">
         <div>Login with Spotify</div>
-      </q-btn>
+      </q-btn><br><br>
     </div>
-    <div v-if="$root.authed==true">
-      authed with {{$root.musicProvider}}
+    <div v-if="$root.authed==true" class="column justify-center">
+      <h4>Currently Playing</h4>
+      <div id="cpctx"></div>
     </div>
   </div>
 </template>
@@ -15,11 +16,160 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios"
+import Konva from "konva"
+import {Plugins} from "@capacitor/core"
+const {Storage} = Plugins;
+const qs = require("querystring");
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  filters: {
+  },
+  data: function() {
+    return {
+      artist: "",
+      songname: ""
+    }
+  },
+  mounted: async function() {
+    //get current playing song
+    const spauth = await Storage.get({key: "spotify_auth"});
+    if(spauth.value != null) {
+      this.$root.spotify = JSON.parse(spauth.value);
+      if(this.$root.spotify.refreshToken != "") {
+        this.$root.authed = true;
+        this.$root.authProvider = "spotify";
+      }
+    }
+    if(this.$root.spotify.refreshToken != "") {
+      this.$q.loading.show();
+      const requestBody = {
+        client_id: "30ff94fb26df46dbab49759ddd32cc1c",
+        grant_type: "refresh_token",
+        refresh_token: this.$root.spotify.refreshToken
+      }
+      
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+      
+      axios.post("https://accounts.spotify.com/api/token", qs.stringify(requestBody), config)
+      .then(async (result) => {
+        this.$root.spotify.accessToken = result.data.access_token;
+        this.$root.spotify.refreshToken = result.data.refresh_token;
+        await Storage.set({key: "spotify_auth", value: JSON.stringify(this.$root.spotify)});
+
+        const cps = (await axios.get("https://api.spotify.com/v1/me/player", {headers: {
+          "Authorization": "Bearer " + this.$root.spotify.accessToken
+        }})).data;
+
+        //console.log(cps);
+        let abarturl = "";
+        let artist = "";
+        let songname = "";
+        try {
+          abarturl = cps.item.album.images[0].url;
+          artist = cps.item.album.artists[0].name;
+          songname = cps.item.name.split("(")[0];
+        } catch(err) {
+          console.log(err);
+        }
+        console.log(abarturl);
+        console.log(artist);
+        console.log(songname);
+        this.artist = artist;
+        this.songname = songname;
+
+        var stage = new Konva.Stage({
+          container: 'cpctx',   // id of container <div>
+          width: 300,
+          height: 400
+        });
+        var layer = new Konva.Layer();
+        var rect = new Konva.Rect({
+          x: 0,
+          y: 0,
+          width: 300,
+          height: 400,
+          fill: "#212121",
+          cornerRadius: 10
+        });
+        layer.add(rect)
+        var imageObj = new Image();
+        imageObj.onload = function () {
+          var image = new Konva.Image({
+            x:150,
+            y:125+25,
+            image: imageObj,
+            width: 250,
+            height: 250,
+            offset: {
+              x: 125,
+              y: 125
+            }
+          });
+
+          // add the shape to the layer
+          layer.add(image);
+          layer.batchDraw();
+        };
+
+        var text = new Konva.Text({
+          x: 25,
+          height: 30,
+          align: "center",
+          y: 150 + 125 + 15,
+          text: songname,
+          fontSize: 30,
+          fontFamily: 'Calibri',
+          fill: 'white'
+        });
+        layer.add(text);
+        text = new Konva.Text({
+          x: 25,
+          height: 30,
+          align: "center",
+          y: 150 + 125 + 15 + 35,
+          text: artist,
+          fontSize:25,
+          fontFamily: 'Calibri',
+          fill: 'white'
+        });
+        layer.add(text);
+
+        imageObj.src = abarturl;
+
+        var logoimageObj = new Image();
+        logoimageObj.onload = function () {
+          var image = new Konva.Image({
+            x:150,
+            y:360,
+            image: logoimageObj,
+            width: 35,
+            height: 35,
+            offset: {
+              x: 12.5,
+              y: 12.5
+            }
+          });
+
+          // add the shape to the layer
+          layer.add(image);
+          layer.batchDraw();
+        };
+
+        logoimageObj.src = "/Spotify_Icon_RGB_White.png";
+
+        stage.add(layer);
+        layer.draw();
+        this.$q.loading.hide();
+      })
+    }
   }
 }
 </script>
